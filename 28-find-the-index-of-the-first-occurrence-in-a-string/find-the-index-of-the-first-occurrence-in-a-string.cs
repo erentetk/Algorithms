60e5b477620d3b8c95024f4cca7c92b4c2ebe6b3
eren tetik <131614656+erentetk@users.noreply.github.com>
2025-09-05 15:36:12 +0300

    Time: 8 ms (30.66%) | Memory: 41.5 MB (7.00%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        int firsindex=0;
+      firsindex=haystack.IndexOf(needle);
+      return firsindex;
+    }
+}
