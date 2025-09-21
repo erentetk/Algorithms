27a57f1e202301d88262850cece76c6af800b02e
eren tetik <131614656+erentetk@users.noreply.github.com>
2025-09-21 14:48:54 +0300

    Time: 2 ms (18.46%) | Memory: 40 MB (41.54%) - LeetSync

--- 1289-day-of-the-week/day-of-the-week.cs
+++ 1289-day-of-the-week/day-of-the-week.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public string DayOfTheWeek(int day, int month, int year) {
-        DateTime today = DateTime.Now;
+
 DateTime specificDate = new DateTime(year, month, day);
 string Result=specificDate.DayOfWeek.ToString();
 return Result;
